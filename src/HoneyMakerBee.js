343db8771105d8b6e2a5a096a5733025e2d39be2
Amanda <albullington@gmail.com>
2018-03-21 18:06:28 -0700

    All tests pass.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  
+  constructor(food, eat, color) {
+  	super(food, eat, color);
+  	this.age = 10;
+  	this.job = 'make honey';
+  	this.honeyPot = 0;
+  }
+
+  makeHoney() {
+  	this.honeyPot += 1;
+  }
+
+  giveHoney() {
+  	this.honeyPot -= 1;
+  }
+
 };
